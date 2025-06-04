f10d742f5152c27f0ca0b8fa470d7c8081b89702
Bashat Parween <68892857+bashat450@users.noreply.github.com>
2025-06-04 14:48:37 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieTicketBooking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult Movies()
+        {
+            return View();
+        }
+
+        public ActionResult MovieDetails()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
